4403ce990a5b8d76d80519ebf7cd4a1c0295a163
Doriano <doriano.bergamin18@gmail.com>
2024-04-17 22:17:40 +0200

    added the services section

--- src/components/services.js
+++ src/components/services.js
@@ -1,3 +1,41 @@
 export default function Services() {
-  return <p></p>;
+  return (
+    <>
+      <span className="cleaning">
+        <strong>Cleaning Service</strong>
+      </span>
+      <img className="cleaningPic" src="/images/cleaning.png" />
+      <p className="textCleaning">
+        Attract more bookings and earn higher revenue with a clean and
+        well-maintained Airbnb property. At Homebound, we have a network of
+        expert property managers across Australia that take the hassle out of
+        Airbnb cleaning. From regular cleaning to deep cleans between guests,
+        we'll ensure your property meets and exceeds your guests' expectations
+        for cleanliness and hygiene. Maximise your Airbnb’s success with a
+        sparkling clean property.{" "}
+      </p>
+      <p className="management">
+        <strong>Property Management</strong>
+      </p>
+      <img className="managementPic" src="/images/management.png" />
+      <p className="textManagement">
+        Discover peace of mind with our comprehensive property management
+        solutions. From meticulous tenant screening and rent collection to
+        proactive maintenance and responsive communication, we handle every
+        aspect of property care with dedication and expertise. Trust us to
+        safeguard your investment while maximizing its value, allowing you to
+        focus on what matters most.
+      </p>
+      <p className="marketing">
+        <strong>Marketing</strong>
+      </p>
+      <img className="marketingPic" src="/images/marketing.png" />
+      <p className="textMarketing">
+        We target tenants with an eye toward your preferences, assembling the
+        perfect mix of marketing channels and messaging. This not only allows us
+        to widen the pool of potential candidates, but to also attract and vet
+        quality occupants.
+      </p>
+    </>
+  );
 }
